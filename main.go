1cd4dd52d1dd4c8adbe312bd3f9cc9a4c68d66f1
Kevin Sarmiento <kevin.sarmiento@uruit.com>
2018-09-11 15:08:13 -0500

    Parsing YAML and converting it into a map complying the MapHandler first argument.

--- main.go
+++ main.go
@@ -13,8 +13,19 @@ func main() {
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Starting the server on :7000")
-	http.ListenAndServe(":7000", mapHandler)
+	http.ListenAndServe(":7000", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
--- urlshort/urlshort.go
+++ urlshort/urlshort.go
@@ -1,6 +1,7 @@
 package urlshort
 
 import (
+	"gopkg.in/yaml.v2"
 	"net/http"
 )
 
@@ -15,23 +16,28 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	return http.HandlerFunc(mapper)
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	parsedYaml, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(yml []byte) ([]map[string]string, error) {
+	var result []map[string]string
+	err := yaml.Unmarshal(yml, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func buildMap(data []map[string]string) map[string]string {
+	builtMap := map[string]string {}
+	for _, elem := range data {
+		builtMap[elem["path"]] = elem["url"]
+	}
+	return builtMap
 }
